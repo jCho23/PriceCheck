70c727825e2622c44d5ce5fc04256aa400de5a80
JuneCho23 <junecho.ucb@gmail.com>
2017-08-29 17:41:11 -0700

    public and private

---
+++ PriceCheck/Services/SearchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+
+
+
+namespace PriceCheck.Services
+{
+    public class SearchService
+    {
+        public static readonly int MinSearchLength = 4;
+
+        private const string Url = "";
+        private HttpClient _client = new HttpClient();
+    }
+}
